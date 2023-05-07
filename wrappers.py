a7b0ca68e9aaa41257c3a22802837095f699fe08
stcoyle <pieradtwo@gmail.com>
2023-05-07 14:17:25 -0500

    Update wrappers.py

--- wrappers.py
+++ wrappers.py
@@ -83,7 +83,7 @@ class FrameStack(gym.Wrapper):
         self.observation_space = gym.spaces.Box(low=0, high=255, shape=(shp[0], shp[1], shp[2] * k), dtype=env.observation_space.dtype)
 
     def reset(self, seed = None, options = None):
-        ob = self.env.reset()
+        ob, info = self.env.reset()
         for _ in range(self.k):
             self.frames.append(ob)
         return self._get_ob()
